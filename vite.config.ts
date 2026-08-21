7f063a4bad0eb8ef37996c390167adae0d376d2d
Sequenter <35529212+sequenter@users.noreply.github.com>
2026-08-21 18:52:55 +0100

    Updated deps, Updated collection items

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,19 @@ import react from '@vitejs/plugin-react';
 import * as path from 'path';
 import { defineConfig } from 'vite';
 
+const dirname = import.meta.dirname;
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@assets': path.resolve(__dirname, './src/assets/'),
-      '@components': path.resolve(__dirname, './src/components/'),
-      '@hooks': path.resolve(__dirname, './src/hooks/'),
-      '@providers': path.resolve(__dirname, './src/providers/'),
-      '@types': path.resolve(__dirname, './src/types.ts'),
-      '@utils': path.resolve(__dirname, './src/utils/')
+      '@assets': path.resolve(dirname, './src/assets/'),
+      '@components': path.resolve(dirname, './src/components/'),
+      '@hooks': path.resolve(dirname, './src/hooks/'),
+      '@providers': path.resolve(dirname, './src/providers/'),
+      '@types': path.resolve(dirname, './src/types.ts'),
+      '@utils': path.resolve(dirname, './src/utils/')
     }
   },
   base: '/Cloggers'
